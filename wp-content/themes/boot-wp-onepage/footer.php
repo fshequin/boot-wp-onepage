581c424ff8c008376d1cd52f1373b9855aee525b
Fred Shequine <fshequin@gmail.com>
2014-08-28 22:02:10 -0400

    initial commit

---
+++ wp-content/themes/boot-wp-onepage/footer.php
@@ -0,0 +1,14 @@
+<div class="container">
+      <div class="row">
+        <div class="col-md-12 footer">
+          <p>&copy; <?php echo date("Y").' - '; bloginfo('name'); ?> - all rights reserved - built by <a href="http://btcny.com" target="_blank">BTC</a> - powered by <a href="http://wordpress.org" target="_blank">Wordpress</a></p>
+        </div>
+      </div>
+    </div>
+    
+
+    
+
+<?php wp_footer(); ?>
+  </body>
+</html>
---
+++ wp-content/themes/boot-wp-onepage/index.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+
+<div class="container">
+      
+<?php if(have_posts()) : ?>
+<?php while(have_posts()) : the_post(); ?>
+
+
+      <div class="row">
+        <div class="col-md-12 post-content">
+          <h2><?php the_title(); ?></h2>
+          <?php the_content(); ?>
+        </div>
+      </div>
+<?php endwhile; ?>
+<?php endif; ?>
+
+</div>
+
+
+<?php get_footer(); ?>

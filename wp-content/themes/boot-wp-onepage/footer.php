ad3d693aa8bcdb2161fe10aaaaa5474315577b47
Fred Shequine <fshequin@gmail.com>
2014-11-25 22:16:15 -0500

    final tweaks
    Signed-off-by: Fred Shequine <fshequin@gmail.com>

--- wp-content/themes/boot-wp-onepage/footer.php
+++ wp-content/themes/boot-wp-onepage/footer.php
@@ -2,7 +2,7 @@
 			<div class="container">
 			    <div class="row">
 			        <div class="col-md-12 footer">
-			          <p>&copy; <?php echo date("Y").' - '; bloginfo('name'); ?> - all rights reserved - built by <a href="http://btcny.com" target="_blank">BTC</a> - powered by <a href="http://wordpress.org" target="_blank">Wordpress</a> - <a href="/wp-admin/" target="_blank"><i class="fa fa-cog"></i></a></p>
+			          <p>&copy; <?php echo date("Y").' - '; bloginfo('name'); ?> - built by <a href="http://btcny.com" target="_blank">BTC</a> - powered by <a href="http://wordpress.org" target="_blank">WordPress</a> - <a href="/wp-admin/" target="_blank"><i class="fa fa-cog"></i></a></p>
 			        </div>
 			   	</div>
 			</div>

c519109a13935a6cb855aa17b4065b8ab7972b07
Fred Shequine <fshequin@gmail.com>
2014-11-26 14:21:16 -0500

    favicons
    Signed-off-by: Fred Shequine <fshequin@gmail.com>

--- wp-content/themes/boot-wp-onepage/header.php
+++ wp-content/themes/boot-wp-onepage/header.php
@@ -4,6 +4,9 @@
   <head>
     <title><?php wp_title(); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- favicons -->
+    <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.ico" type="image/x-icon" />
     <!-- Bootstrap -->
     <link href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css" rel="stylesheet">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
